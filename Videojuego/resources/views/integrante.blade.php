2cadc70ee922c897b938d399e1a3f0ead1ea3150
Jorge Andres <A01209043@itesm.mx>
2020-09-30 19:11:26 -0500

    Configuración Actividad Laravel

---
+++ Videojuego/resources/views/integrante.blade.php
@@ -0,0 +1,22 @@
+@extends("layout")
+@section("page_title", "Equipo LDAW")
+
+@section("main_content")
+<div class="container">
+	<div class="row justify-content-center">
+		<div class="col-md-3">
+			<h1> Jorge </h1>
+		</div>
+		<div class="col-md-3">
+			<h1> Victor </h1>
+		</div>
+		<div class="col-md-3">
+			<h1> Randy </h1>
+		</div>
+		<div class="col-md-3">
+			<h1> Lalo </h1>
+		</div>
+	</div>
+
+</div>
+@endsection
--- Videojuego/routes/web.php
+++ Videojuego/routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'integrante');
+
+Route::view('/jorge', 'jorge');
+
+Route::view('/victor', 'victor');
+
+Route::view('/lalo', 'lalo');
+
+Route::view('/randy', 'randy');
+
+
+
